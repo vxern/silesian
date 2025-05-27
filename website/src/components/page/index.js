9c42beadbe695cc63e6917c5f71eae088e86dded
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-05-27 20:34:12 +0100

    feat: Add missing import.

--- website/src/components/page/index.js
+++ website/src/components/page/index.js
@@ -2,5 +2,6 @@ import Root from "./page.svelte";
 import Card from "./page-card.svelte";
 import Contents from "./page-contents.svelte";
 import Header from "./page-header.svelte";
+import Title from "./page-title.svelte";
 
-export default { Root, Card, Contents, Header };
+export default { Root, Card, Contents, Header, Title };
