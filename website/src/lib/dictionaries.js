ad2263b53bf5daecbd738d4097798210994562c5
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-05-29 21:55:20 +0100

    feat: Add more dictionaries.

--- website/src/lib/dictionaries.js
+++ website/src/lib/dictionaries.js
@@ -1,6 +1,133 @@
 export default {
-  silling: {
+  "kulik": {
     link: "https://silling.org/slownik",
     licence: "CC BY-NC-SA 4.0"
+  },
+  "wiktionary": {
+    link: "https://en.wiktionary.org",
+    licence: "CC BY-SA 4.0",
+  },
+  "neblik.orliński": {
+    link: "https://dykcjonorz.eu",
+    licence: "proprietary",
+  }
+  "syniawa": {
+    link: null,
+    licence: "proprietary",
+  },
+  "wencel/słownik": {
+    link: "https://www.math.uni.wroc.pl/~wencel/sil/slownik_sp.pdf",
+    licence: "proprietary",
+  },
+  "kosmała": {
+    link: "https://sbc.org.pl/dlibra/publication/648390",
+    licence: "proprietary",
+  },
+  "lerch-wójcik": {
+    link: "https://bonclok.pl/slowniczek-wyrazow-slaskich.html",
+    licence: "proprietary",
+  },
+  // TODO(vxern): What's this guy's real name?
+  "fazi": {
+    link: "https://www.slownikslaski.pl",
+    licence: "proprietary",
+  },
+  "markowski": {
+    link: "https://www.antryj.pl/category/slownik/",
+    licence: "proprietary",
+  },
+  "dygacz:słownik": {
+    link: "https://www.sbc.org.pl/dlibra/publication/880766/edition/802420",
+    licence: "public",
+  },
+  "dygacz:wyrazy": {
+    link: "https://www.sbc.org.pl/dlibra/publication/861664/edition/788851",
+    licence: "public",
+  },
+  "przywara": {
+    link: "https://sbc.org.pl/dlibra/publication/68791/edition/64871",
+    licence: "public",
+  },
+  "lazik": {
+    link: "http://www.dr-lazik.de/",
+    licence: "proprietary",
+  },
+  "parczewska": {
+    // TODO(vxern): Add the original link.
+    link: "https://silling.org/slownik/Aneks:Parczewska_-_Przyczynki_do_s%C5%82ownika_gwary_%C5%9Bl%C4%85skiej",
+    licence: "proprietary",
+  },
+  // TODO(vxern): Who's the original author?
+  "nasza.trzynastka": {
+    link: "https://www.naszatrzynastka.pl/dokumenty/uczniowie/moj-region-moja-ojczyzna.html",
+    licence: "proprietary",
+  },
+  "olesch:1": {
+    link: "https://sbc.org.pl/dlibra/publication/958233",
+    licence: "proprietary",
+  },
+  "olesch:2": {
+    link: "https://sbc.org.pl/dlibra/publication/958234",
+    licence: "proprietary",
+  },
+  "wencel:rojśliny": {
+    link: "https://www.math.uni.wroc.pl/~wencel/sil/rojsliny.pdf",
+    licence: "proprietary",
+  },
+  "zaranie:1/1929": {
+    // TODO(vxern): Add the original link.
+    link: "https://silling.org/slownik/Aneks:S%C5%82ownik_-_Zaranie_%C5%9Al%C4%85skie_nr_1/1929",
+    licence: "public",
+  },
+  "zaranie:2/1929": {
+    // TODO(vxern): Add the original link.
+    link: "https://silling.org/slownik/Aneks:S%C5%82ownik_-_Zaranie_%C5%9Al%C4%85skie_nr_2/1929",
+    licence: "public",
+  },
+  "zaranie:3/1929": {
+    // TODO(vxern): Add the original link.
+    link: "https://silling.org/slownik/Aneks:S%C5%82ownik_-_Zaranie_%C5%9Al%C4%85skie_nr_3/1929",
+    licence: "public",
+  },
+  "zaranie:1/1930": {
+    // TODO(vxern): Add the original link.
+    link: "https://silling.org/slownik/Aneks:S%C5%82ownik_-_Zaranie_%C5%9Al%C4%85skie_nr_1/1930",
+    licence: "public",
+  },
+  "zaranie:2/1930": {
+    // TODO(vxern): Add the original link.
+    link: "https://silling.org/slownik/Aneks:S%C5%82ownik_-_Zaranie_%C5%9Al%C4%85skie_nr_2/1930",
+    licence: "public",
+  },
+  // TODO(vxern): Who made this?
+  "zestawienie.gatunków.roślin": {
+    link: "https://instytutkorfantego.pl/wp-content/uploads/2020/12/ZESTAWIENIE-GATUNK%C3%93W-RO%C5%9ALIN-ROSN%C4%84CYCH-W-OGR%C3%93DKACH-PRZYDOMOWYCH.pdf",
+    licence: "proprietary",
+  },
+  "wencel:grziby": {
+    link: "https://www.math.uni.wroc.pl/~wencel/sil/grziby.pdf",
+    licence: "proprietary",
+  },
+  "gładysz:zdobnictwo": {
+    link: "https://pbc.gda.pl/dlibra/publication/1165/edition/8684",
+    licence: "proprietary",
+  },
+  // TODO(vxern): Who wrote this?
+  "euro-met": {
+    link: "https://euro-met.pl/informator/slaski-slownik",
+    licence: "proprietary",
+  },
+  // TODO(vxern): Who wrote this? Is this worth including?
+  "ukłŏd.ôkresowy.elymyntōw": {
+    link: "https://canov.jergym.cz/vyhledav/varian33/slez/slp.htm",
+    licence: "proprietary",
+  },
+  "kallus": {
+    link: "https://lubimyczytac.pl/ksiazka/4439064/slownik-g-rnosl-nskij-g-dki",
+    licence: "proprietary",
+  },
+  "wyderka": {
+    link: "https://instytutslaski.pl/slownik-gwar-slaskich/",
+    licence: "proprietary",
   },
 }
