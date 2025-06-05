2d2ce4402bac775aef62fdfc1acaa211b7ba7b90
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-06-05 20:51:38 +0100

    fix: Variadic classes being a pain.

--- website/src/components/navigation/navigation-button.svelte
+++ website/src/components/navigation/navigation-button.svelte
@@ -1,16 +1,18 @@
 <script>
-  const { children, highlighted, onclick } = $props();
+  import { clsx } from "clsx/lite";
 
-  // TODO(vxern): Fix the classes.
+  const { children, highlighted, onclick } = $props();
 </script>
 
 <button
-  class="flex gap-x-2 items-center font-bold text-xl"
-  class:cursor-pointer={onclick}
-  class:text-yellow-400={highlighted}
-  class:text-zinc-400={!highlighted}
-  class:[hover:text-yellow-300]={!!onclick && highlighted}
-  class:[hover:text-zinc-300]={onclick && !highlighted}
+  class={clsx(
+    "flex gap-x-2 items-center font-bold text-xl",
+    onclick && "cursor-pointer",
+    highlighted && "text-yellow-400",
+    !highlighted && "text-zinc-400",
+    !!onclick && highlighted && "hover:text-yellow-300",
+    onclick && !highlighted && "hover:text-zinc-300"
+  )}
   {onclick}
 >
   {@render children?.()}
