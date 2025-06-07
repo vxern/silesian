ada32555d83dcce50fcdc3c6e85a676c399b82e0
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-06-07 10:12:07 +0100

    feat: Create loading element.

---
+++ website/src/components/meta/loading.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { Diamonds } from "svelte-loading-spinners";
+</script>
+
+<!-- TODO(vxern): Can we get legit Tailwind sizing here? -->
+<Diamonds color="var(--color-yellow-400)" size="64" />
