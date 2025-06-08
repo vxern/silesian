02c4333611f2ac651fff14704e1560066b62716b
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-06-08 15:48:58 +0100

    refactor: Rename the Twitter social to X.

--- website/src/routes/socials/x/+page.server.js
+++ website/src/routes/socials/x/+page.server.js
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import constants from "$lib/constants";
 
 export function load() {
-	redirect(308, constants.links.twitter);
+	redirect(308, constants.links.x);
 }
