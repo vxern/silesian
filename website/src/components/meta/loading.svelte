9c0c0bf9476d4ad82b4d0d1cbb69aa328a8dac2a
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-06-08 13:01:58 +0100

    feat: Decrease the size of the loading element and make it slower.

--- website/src/components/meta/loading.svelte
+++ website/src/components/meta/loading.svelte
@@ -3,4 +3,4 @@
 </script>
 
 <!-- TODO(vxern): Can we get legit Tailwind sizing here? -->
-<Diamonds color="var(--color-yellow-400)" size="64" />
+<Diamonds color="var(--color-yellow-400)" size="56" duration="2s" />
