a1ee8a227c68550d2a30ee36fa56fb99ef82066c
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-06-09 21:10:38 +0100

    feat: Create page to create a new entry.

---
+++ website/src/routes/editor/new/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { m } from "$lib/paraglide/messages";
+  import Page from "../../../components/page/index.js";
+  import NavigationSection from "../../../components/navigation/navigation-section.svelte";
+  import constants from "$lib/constants";
+</script>
+
+<svelte:head>
+  <meta
+    name="description"
+    content={m["routes.editor.new.description"]({
+      project_name: constants.project.name,
+    })}
+  />
+  <title>
+    {m["title"]({
+      project_name: constants.project.name,
+      page_title: m["routes.editor.new.title"](),
+    })}
+  </title>
+</svelte:head>
+
+<NavigationSection />
+
+<Page.Root>
+  <Page.Header>
+    <Page.Title title={m["routes.editor.new.title"]()} />
+  </Page.Header>
+</Page.Root>
