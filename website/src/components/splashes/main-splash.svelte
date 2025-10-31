0bbdff92d63dbcbad4fa2f6fa7073c41c69b53e0
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-10-31 13:05:00 +0000

    feat: Create `MainSplash`.

---
+++ website/src/components/splashes/main-splash.svelte
@@ -0,0 +1,28 @@
+<script>
+  import constants from "$lib/constants/core";
+  import { m } from "$lib/paraglide/messages";
+
+  // TODO(vxern): Je ku tymu lepszy spusōb?
+  const splashKeys = [
+    m["routes.home.splashes.one"](),
+    m["routes.home.splashes.two"](),
+    m["routes.home.splashes.three"](),
+    m["routes.home.splashes.four"](),
+    m["routes.home.splashes.five"](),
+    m["routes.home.splashes.six"](),
+    m["routes.home.splashes.seven"](),
+    m["routes.home.splashes.eight"](),
+  ];
+  const splash = splashKeys[Math.floor(Math.random() * splashKeys.length)];
+</script>
+
+<section class="flex-1 flex flex-col gap-y-8 items-center">
+  <article>
+    <h1>
+      <span class="text-8xl text-yellow-400 font-bold">
+        {constants.project.name}
+      </span>
+    </h1>
+    <h2 class="text-2xl text-blue-500">{splash}</h2>
+  </article>
+</section>
