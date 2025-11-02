d73a969684436fc265f2eacdb7d5d8d05f811634
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-11-02 00:00:23 +0000

    feat: Extract editor into its own element.

--- website/src/components/form/editor-element.svelte
+++ website/src/components/form/editor-element.svelte
@@ -2,20 +2,20 @@
   import Label from "./label.svelte";
   import { renderMarkdown } from "../../helpers/markdown.js";
 
-  const { name, label, placeholder, previewMessage } = $props();
+  const { name, label, description, previewMessage } = $props();
 
   let contents = $state("");
   let renderedContents = $derived(renderMarkdown(contents));
 </script>
 
 <section class="flex-1 flex flex-col gap-y-1">
-  <Label {name} {label} description={placeholder} />
+  <Label {name} {label} {description} />
   <section class="flex flex-1">
     <textarea
       type="text"
       {name}
       rows="16"
-      class="flex-1 bg-zinc-800 outline-1 outline-zinc-600 text-zinc-300 placeholder:text-zinc-500 p-4 rounded-l-lg text-sm font-mono h-160"
+      class="flex-1 bg-zinc-800 outline-1 outline-zinc-600 text-zinc-300 p-4 rounded-l-lg text-sm font-mono h-160"
       bind:value={contents}
     ></textarea>
     <section
