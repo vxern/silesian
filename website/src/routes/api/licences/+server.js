feb36f1d6c4daeeaaa4a11a9149945586efd03bf
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-11-05 21:38:42 +0000

    feat: Get rid of licence editing.

--- website/src/routes/api/licences/+server.js
+++
@@ -1,9 +0,0 @@
-import { json } from "@sveltejs/kit";
-import { db } from "$lib/database.server";
-import { licences } from "$lib/database/schema";
-
-// TODO(vxern): Do not return deleted licences.
-
-export async function GET() {
-  return json(await db.select().from(licences));
-}
