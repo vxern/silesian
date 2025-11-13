642f69bc5307b86ba2a54f873d24f78e1e193466
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-11-13 22:45:20 +0000

    feat: Add colour support to badges and buttons.

--- website/src/components/interactions/badge.svelte
+++ website/src/components/interactions/badge.svelte
@@ -1,9 +1,31 @@
 <script>
+  import { clsx } from "clsx/lite";
+
   const { text, colour = "red" } = $props();
+
+  let colourClasses;
+  switch (colour) {
+    case "yellow":
+      colourClasses =
+        "text-yellow-200 bg-yellow-600 outline-2 outline-yellow-500";
+      break;
+    case "zinc":
+      colourClasses = "text-zinc-100 bg-zinc-500 outline-2 outline-zinc-400";
+      break;
+    case "blue":
+      colourClasses = "text-blue-100 bg-blue-500 outline-2 outline-blue-400";
+      break;
+    case "red":
+      colourClasses = "text-red-100 bg-red-500 outline-2 outline-red-400";
+      break;
+  }
 </script>
 
 <section
-  class="absolute top-[-0.5rem] right-[-0.5rem] text-sm rounded-full px-1 bg-{colour}-500 text-white inline-flex items-center justify-center"
+  class={clsx(
+    "absolute top-[-0.5rem] right-[-0.5rem] text-xs rounded-full px-1 inline-flex items-center justify-center",
+    colourClasses
+  )}
 >
   {text}
 </section>
--- website/src/components/interactions/button.svelte
+++ website/src/components/interactions/button.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { clsx } from "clsx/lite";
+
   const {
     icon: Icon,
     colour = "blue",
@@ -6,10 +8,37 @@
     children,
     ...props
   } = $props();
+
+  let colourClasses;
+  switch (colour) {
+    case "yellow":
+      colourClasses =
+        "outline-yellow-400 bg-yellow-600 hover:bg-yellow-500 text-yellow-100 hover:text-white";
+      break;
+    case "blue":
+      colourClasses =
+        "outline-blue-500 bg-blue-700 hover:bg-blue-600 text-blue-200 hover:text-blue-100";
+      break;
+    case "green":
+      colourClasses =
+        "outline-green-500 bg-green-700 hover:bg-green-600 text-green-200 hover:text-green-100";
+      break;
+    case "red":
+      colourClasses =
+        "outline-red-500 bg-red-700 hover:bg-red-600 text-red-200 hover:text-red-100";
+      break;
+    case "zinc":
+      colourClasses =
+        "outline-zinc-500 bg-zinc-700 hover:bg-zinc-600 text-zinc-200 hover:text-zinc-100";
+      break;
+  }
 </script>
 
 <button
-  class="relative cursor-pointer flex items-center gap-x-1 outline-1 outline-{colour}-500 bg-{colour}-700 hover:bg-{colour}-600 text-{colour}-200 hover:text-{colour}-100 rounded-lg text-md font-bold p-2"
+  class={clsx(
+    "relative cursor-pointer flex items-center gap-x-1 outline-1 rounded-lg text-md font-bold p-2",
+    colourClasses
+  )}
   {type}
   {...props}
 >
