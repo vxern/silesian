b173ee2a16d0d53182f0440481c3908612404417
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-11-15 22:29:03 +0000

    feat: Add colour support to the loading component.

--- website/src/components/meta/loading.svelte
+++ website/src/components/meta/loading.svelte
@@ -1,8 +1,23 @@
 <script>
   import { Diamonds } from "svelte-loading-spinners";
+
+  const { colour } = $props();
+
+  let colourClass;
+  switch (colour) {
+    case "red":
+      colourClass = "var(--color-red-400)";
+      break;
+    case "yellow":
+      colourClass = "var(--color-yellow-400)";
+      break;
+    case "blue":
+      colourClass = "var(--color-blue-400)";
+      break;
+  }
 </script>
 
 <section class="flex justify-center">
   <!-- TODO(vxern): Can we get legit Tailwind sizing here? -->
-  <Diamonds color="var(--color-yellow-400)" size="56" duration="2s" />
+  <Diamonds color={colourClass} size="56" duration="2s" />
 </section>
