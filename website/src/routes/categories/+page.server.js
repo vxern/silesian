917f0c2c95bd75f8c9c693f7a05b87804c255337
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-11-24 16:48:56 +0000

    style: Switch to `db.$count`.

--- website/src/routes/categories/+page.server.js
+++ website/src/routes/categories/+page.server.js
@@ -5,8 +5,8 @@ import { eq, count } from 'drizzle-orm';
 export const load = async () => {
   return {
     // TODO(vxern): Make sure to filter by the user.
-    draftCount: db.select({ count: count() }).from(categories).where(eq(categories.status, "draft")).then((results) => results.at(0).count),
-    pendingCount: db.select({ count: count() }).from(categories).where(eq(categories.status, "pending")).then((results) => results.at(0).count),
+    draftCount: db.$count(categories, eq(categories.status, "draft")),
+    pendingCount: db.$count(categories, eq(categories.status, "pending")),
     categories: await db.query.categories.findMany({ where: (categories, { eq }) => eq(categories.status, "published") }),
   };
 };
