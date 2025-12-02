131bfe685421c328918b2bfbbacad1bb0fba6688
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-12-02 21:38:15 +0000

    feat: Bump package versions.

--- website/src/auth.js
+++ website/src/auth.js
@@ -1,15 +1,19 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import Discord from "@auth/sveltekit/providers/discord"
-import GitHub from "@auth/sveltekit/providers/github"
-import Google from "@auth/sveltekit/providers/google"
-import LinkedIn from "@auth/sveltekit/providers/linkedin"
-import Twitter from "@auth/sveltekit/providers/twitter"
+import { SvelteKitAuth } from "@auth/sveltekit";
+import Discord from "@auth/sveltekit/providers/discord";
+import GitHub from "@auth/sveltekit/providers/github";
+import Google from "@auth/sveltekit/providers/google";
+import LinkedIn from "@auth/sveltekit/providers/linkedin";
+import Twitter from "@auth/sveltekit/providers/twitter";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-  providers: [Discord(), GitHub(), Google(), LinkedIn(), Twitter()],
-  trustHost: true,
-  pages: {
-    signIn: "/login",
-    signOut: "/logout",
-  },
+	providers: [Discord(), GitHub(), Google(), LinkedIn(), Twitter()],
+	session: {
+		strategy: "jwt",
+		maxAge: 60 * 60 * 24 * 365, // 1 year
+	},
+	trustHost: true,
+	pages: {
+		signIn: "/login",
+		signOut: "/logout",
+	},
 });
