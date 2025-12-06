afa774b68ec3d097f80e613d54d9ce0ae9ec7298
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-12-06 18:07:24 +0000

    style: Rename `created_at` and `updated_at` to something clearer.

--- website/src/lib/database/tables/search-frequencies.js
+++ website/src/lib/database/tables/search-frequencies.js
@@ -5,10 +5,8 @@ export const searchFrequencies = pgTable("search_frequencies", {
   id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
   lemma: text().notNull(),
   count: integer().default(0).notNull(),
-  // Date of the first search for this word.
-  created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
-  // Date of the latest search for this word.
-  updated_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  first_searched_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  most_recently_searched_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
 }, (t) => [
   unique().on(t.lemma),
   check("lemma_not_empty_check", sql`${t.lemma} <> ''`),
