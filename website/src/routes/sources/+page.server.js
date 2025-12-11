f10c24d095d9f99826cbe948cf9f51ed1b132f5f
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-12-11 13:49:51 +0000

    feat: Bring the base sources route up to speed.

--- website/src/routes/sources/+page.server.js
+++ website/src/routes/sources/+page.server.js
@@ -1,21 +1,72 @@
 import { db } from "$lib/database.server";
-import { sources } from "$lib/database/schema";
-import { eq, count } from 'drizzle-orm';
+import { sources, versions, authorsToSources, authors } from "$lib/database/schema";
+import { and, ne, eq, count } from 'drizzle-orm';
 
 export const load = async () => {
   return {
-    // TODO(vxern): Make sure to filter by the user.
-    draftCount: db.$count(sources, eq(sources.status, "draft")),
-    pendingCount: db.$count(sources, eq(sources.status, "pending")),
-    sources: await db.query.sources.findMany({
-      where: (sources, { eq }) => eq(sources.status, "published"),
-      with: {
-        authors: {
-          with: {
-            author: true
-          },
-        },
-      },
-    }),
+    draftCount: getDraftCount(),
+    pendingCount: getPendingCount(),
+    sources: await getPublishedSources(),
   };
 };
+
+/** Performs 1 query. */
+function getDraftCount() {
+  return db
+    .select({ count: count() })
+    .from(sources)
+    .withVersions()
+    .where(
+      and(
+        // TODO(vxern): Set the right author.
+        eq(versions.author_id, 2),
+        eq(sources.deleted, false),
+        eq(sources.status, "draft"),
+      ),
+    )
+    .then((results) => results.at(0).count);
+}
+
+/** Performs 1 query. */
+function getPendingCount() {
+  return db
+    .select({ count: count() })
+    .from(sources)
+    .withVersions()
+    .where(
+      and(
+        // TODO(vxern): Set the right author.
+        ne(versions.author_id, 2),
+        eq(sources.deleted, false),
+        eq(sources.status, "pending"),
+      ),
+    )
+    .then((results) => results.at(0).count);
+}
+
+/** Performs 1 query. */
+function getPublishedSources() {
+  return db
+    .select({ sources, authors })
+    .from(sources)
+    .withVersions()
+    .where(
+      and(
+        eq(sources.deleted, false),
+        eq(sources.status, "published"),
+      ),
+    )
+    .leftJoin(authorsToSources, eq(authorsToSources.source_id, sources.id))
+    .leftJoin(authors, eq(authors.id, authorsToSources.author_id))
+    .then(
+      (results) => Object.values(Object.groupBy(results, ({ sources }) => sources.id)).map(
+        (results) => {
+          const source = results[0].sources;
+
+          source.authors = results.map((result) => result.authors).filter((author) => author);
+
+          return source;
+        }
+      ),
+    );
+}
