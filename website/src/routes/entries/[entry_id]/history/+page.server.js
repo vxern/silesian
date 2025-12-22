f215697d4683eac399b44ae19eb038e907f56e15
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-12-22 17:43:24 +0000

    feat: Add a history page for entries.

---
+++ website/src/routes/entries/[entry_id]/history/+page.server.js
@@ -0,0 +1,38 @@
+import { db } from "$lib/database.server";
+
+export const load = async ({ params }) => {
+  // TODO(vxern): Kick the user out if they haven't got permission.
+  // TODO(vxern): Validate the parameter.
+  const [entry, versions] = await Promise.all([
+    getEntry({ id: params.entry_id }),
+    getVersions({ id: params.entry_id }),
+  ]);
+
+  return { entry, versions };
+};
+
+function getEntry({ id }) {
+  return db.query.entries.findFirst({
+    where: {
+      id,
+      // TODO(vxern): Re-enable this.
+      // status: { ne: "draft" },
+      deleted: false,
+    },
+  });
+}
+
+function getVersions({ id }) {
+  return db.query.versions.findMany({
+    where: {
+      versionable_type: "entries",
+      versionable_id: id,
+    },
+    with: {
+      author: true,
+    },
+    orderBy: {
+      version: "desc",
+    },
+  });
+}
---
+++ website/src/routes/entries/[entry_id]/history/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { m } from "$lib/paraglide/messages";
+  import { goto } from "$app/navigation";
+  import Page from "../../../../components/page/index.js";
+  import NavigationSection from "../../../../components/navigation/navigation-section.svelte";
+  import VersionTable from "../../../../components/versions/version-table.svelte";
+  import BackButton from "../../../../components/interactions/back-button.svelte";
+  import constants from "$lib/constants/core";
+
+  // TODO(vxern): Kick the user out if they don't have permission to see the page.
+
+  const { data } = $props();
+</script>
+
+<svelte:head>
+  <meta
+    name="description"
+    content={m["routes.entries.[entry_id].history.description"]()}
+  />
+  <title>
+    {m["title"]({
+      project_name: constants.project.name,
+      page_title: m["routes.entries.[entry_id].history.title"]({
+        entry_lemma: data.entry.lemma,
+      }),
+    })}
+  </title>
+</svelte:head>
+
+<NavigationSection />
+
+<Page.Root>
+  <Page.Header>
+    <Page.Title
+      title={m["routes.entries.[entry_id].history.title"]({
+        entry_lemma: data.entry.lemma,
+      })}
+    />
+  </Page.Header>
+  <Page.Divider />
+  <Page.Contents>
+    <Page.Actions>
+      {#if data.entry.status === "published"}
+        <BackButton onclick={() => goto(`/entries#${data.entry.id}`)} />
+      {:else if data.entry.status === "pending"}
+        <BackButton onclick={() => goto(`/entries/pending#${data.entry.id}`)} />
+      {/if}
+    </Page.Actions>
+    <VersionTable
+      object={data.entry}
+      versions={data.versions}
+      types={{
+        country: "country",
+      }}
+      translateProperty={(property) => m[`routes.entries.form.${property}`]()}
+    />
+  </Page.Contents>
+</Page.Root>
