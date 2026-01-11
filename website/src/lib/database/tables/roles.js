359b2723082e66db624fc003e076178b37bc2552
Dorian Oszczęda <vxern@wordcollector.co.uk>
2026-01-11 13:10:43 +0000

    feat: Add the permissions enum.

--- website/src/lib/database/tables/roles.js
+++ website/src/lib/database/tables/roles.js
@@ -1,7 +1,7 @@
 import { defineRelationsPart } from "drizzle-orm";
 import { pgTable, bigint, text, uniqueIndex, timestamp, unique } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
-import { reviewDecisionsEnum } from "../enums/review-decisions";
+import { permissionsEnum } from "../enums/permissions";
 import { versions } from "./versions";
 import { users } from "./users";
 import * as schema from "../schema";
@@ -9,7 +9,7 @@ import * as schema from "../schema";
 export const roles = pgTable("roles", {
   id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
   name: text().notNull(),
-  permissions: text().array().default([]).notNull(),
+  permissions: permissionsEnum().array().default([]).notNull(),
   created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
   updated_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
 }, (t) => [
